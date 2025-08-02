940f8f5786418e4ada1307c9c16587bc30c5f785
Govind Khandelwal <khandelwalgov@gmail.com>
2025-08-02 20:23:02 +0530

    solving cookie issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import copy from 'rollup-plugin-copy'
 
+// https://vite.dev/config/
 export default defineConfig({
- plugins: [
-  react(),
-  copy({
-   targets: [
-    { src: 'staticwebapp.config.json', dest: 'dist' }
-   ],
-   hook: 'writeBundle'
-  })
- ]
+  plugins: [react()],
 })
